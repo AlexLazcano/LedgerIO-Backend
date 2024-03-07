8c15d8926b8658f8be2f5371fe6767dee2478721
Alex Lazcano <alazcano2000@gmail.com>
2024-03-07 14:43:19 -0800

    Removed kube services files as they are moved to their own repo

--- transaction-service/routers/transactions.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const transactionController = require('../controllers/transactions.controller');
-
-router.post('/', transactionController.createTransaction);
-router.get('/:userId', transactionController.getTransactionsBySender);
-
-module.exports = router;
--- user-service/routers/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/user.controller');
-
-router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-
-module.exports = router;
